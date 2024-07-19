8d0469fc4465115686fbdd5f31a8610041178911
Om Nigam <om2021064@akgec.ac.in>
2024-07-19 10:10:10 +0530

    pass funtion

---
+++ #26PrintINRange.py
@@ -0,0 +1,12 @@
+# Print the multiplication table of a number n 
+
+n = int(input("Enter the num : "))
+
+for i in range(n , n*10+1, n):
+    print(i)
+
+
+# or 
+
+for i in range(1, 11):
+    print(i*n)
---
+++ #27Pass.py
@@ -0,0 +1,13 @@
+# Pass keyword is a null statement that does nothing. It is used as a placeholder for future code.
+# It allows you to define a block of code that you plan to implement later, without causing syntax errors.
+
+for el in range(10):
+    pass  # Placeholder for future code - This loop does nothing right now but is set up for future use.
+
+# The 'pass' statement is useful for creating minimal code stubs and is generally used in:
+# 1. Loops: Where the loop body needs to be defined later.
+# 2. Functions: When you want to define the function without implementing it yet.
+# 3. Classes: To define class methods or attributes that will be implemented later.
+# 4. Exception handling: In try-except blocks where you want to catch an exception but not take any action.
+
+print("Hello")  # This line prints "Hello" to the console.
